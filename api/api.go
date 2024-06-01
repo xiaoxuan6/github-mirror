613b6493d341f8b7e0d53a494eca7a84f7be5991
xiaoxuan6 <1527736751@qq.com>
2024-06-01 13:27:59 +0800

    fix: Update api.go

--- api/api.go
+++ api/api.go
@@ -1,1 +1,9 @@
 package api
+
+import "net/http"
+
+func Api(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "application/json;charset=utf-8")
+
+    _, _ = w.Write([]byte(`{"code":200, "msg": "ok"}`))
+}
